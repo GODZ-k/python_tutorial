c0b2db0b85d5e526173c6a8147bc2f5e598fa894
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-08-06 19:18:34 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,30 @@
+#function ---------------------------------------------------------------------------
+
+# function definition
+# def demo(a,b=5): # b=5 is the default value if user will not define the value of b
+    # c=a+b
+    # print(c)
+    # return c # it stores the value of the result for future use
+
+# function calling
+# n1=10
+# n2=10
+# demo(n1,n2)
+# output=demo(n1,n2)
+# print(output)
+
+# module-------------------------------------------------------------------------------
+#import module
+# import module1 as m
+# from module1 import add  # impore specific function
+# from module1 import *  # import all function of the module 
+
+# print(module1.add(10,20))
+# a=module1.add(10,20)
+# print(a)
+# print(m.add(10,20))
+# print(m.mul(10,20))
+# print(add(10,10))
+
+# from module1 import mul
+# print(mul(10,10))
---
+++ mathfun.py
@@ -0,0 +1,18 @@
+import math
+# x=12.4
+# print(math.ceil(x))  # celling value
+
+# print(math.floor(x))  # floor value
+
+# x=-10
+# print(math.fabs(x))  # convert negative value into positive value
+
+# x=5
+# print(math.factorial(5))   # give the factorial of the value
+
+# l=[10,20,30]
+# print(math.fsum(l))  # give the sum of the values
+
+
+# print(math.sqrt(81))  # give the square root of the value
+
