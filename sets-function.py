6f10114e6a1514dc910c72e64fe15f718e53588f
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-08-06 14:58:00 +0530

    Add files via upload

---
+++ sets-function.py
@@ -0,0 +1,48 @@
+# s={10,20,30,10}
+# print(s)
+# set() use to convert list into set
+# add() use to add item in the set
+# pop() use to remove item into the set(random remove)
+# remove() use to remove specific item into the set(if removed item does not eists in the set it will raise the error)
+# clear()  use to clear the set
+# discard() use to remove specific item into the set(if removed item does not eists in the set it will not raise the error)
+# update() use to update the set(add another list or set in the set)(use to merge the set or list)
+# set()---------------------------------
+# l=[10,20,30]
+# s=set(l)
+# print(s)
+
+# add()------------------------------
+# s={10,20,30,40,20}
+# s.add(80)
+# print(s)
+
+
+# pop()--------------------------------
+# s={10,20,30,40}
+# s.pop()
+# print(s)
+# print(s.pop())
+
+# remove()--------------------------------
+# s={10,20,30,40}
+# s.remove(10)
+# print(s)
+
+# discard()--------------------------
+# s={10,20,30,40,20}
+# s.discard(10)
+# print(s)
+
+# clear()------------------
+# s={10,20,30}
+# s.clear()
+# print(s)
+
+# update()-----------------------
+# s={10,20,30}
+# # l=[60,70,10]
+# s1={23,35,54,3,51,10}
+# s.update(s1)
+# print(s)
+
---
+++ tuples.py
@@ -0,0 +1,9 @@
+# t=(10,20,30,40)   # tuple is a immutable datatype means that we can not detete insert and modify the tuple after define .
+# if tuple contains only one value it can not be tuple it will be int or string .
+# a=max(t)
+# a=min(t)
+# a=t.count(10)
+# a=t.index(40)
+# a=sum(t)  # sum of all the values of the tuples
+# a=sum(t,10)  # after sum add additional value 10 in the result
+# print(a)
