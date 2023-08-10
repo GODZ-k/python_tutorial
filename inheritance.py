bd9550dbf5cec7ef079eb66779b2a7109faecfcf
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-08-10 11:55:30 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,49 @@
+# single inheritance -----------------------------------
+# class A:
+#     def classA(self):
+#         print('class A')
+
+# class B(A):
+#     def classB(self):
+#      print('class B')
+
+# a=B()
+# a.classB()
+# a.classA()
+
+# multilevel inheritance ------------------------------
+# class A:
+#     def classA(self):
+#         print('class A')
+
+# class B(A):
+#     def classB(self):
+#      print('class B')
+
+# class C(B):
+#     def classC(self):
+#       print('class C')
+
+# a=C()
+# a.classB()
+# a.classA()
+# a.classC()
+
+# multiple inheritance -------------------------------
+class A:
+    def classA(self):
+        print('class A')
+
+class B:
+    def classB(self):
+     print('class B')
+
+class C(A,B):
+    def classC(self):
+      print('class C')
+
+a=C()
+# a=B()
+a.classB()
+a.classA()
+a.classC()
---
+++ json.py
@@ -0,0 +1,15 @@
+import json
+# d={
+#     'name':'tanmay',
+#     'age':22
+# }
+# e=json.dump(d)
+# print(e)
+
+# d='{
+#      'name':'tanmay',
+#      'age':22
+#  }'
+
+# y=json.load(d)
+# print(y)## Working on the project
